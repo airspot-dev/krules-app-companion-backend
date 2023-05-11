6ee13c14b5cb06c09219e6b68c7820ca617b759e
Zenzo42 <lorenzo@airspot.tech>
2023-05-11 16:17:40 +0200

    add jsonserilazer to urmet message sent

--- custom/state-changes-dispatcher/ruleset.py
+++ custom/state-changes-dispatcher/ruleset.py
@@ -5,6 +5,19 @@ from krules_core.event_types import *
 from krules_core.models import Rule
 from state_changes_functions.filters import *
 import re
+import json
+
+
+class JsonDecode(ProcessingFunction):
+
+    def execute(self, properties):
+
+        for prop in properties:
+            if prop in self.payload["value"]:
+                self.payload["value"][prop] = json.loads(self.payload["value"][prop])
+            if prop in self.payload["old_value"]:
+                self.payload["old_value"][prop] = json.loads(self.payload["old_value"][prop])
+
 
 
 rulesdata = [
@@ -12,12 +25,15 @@ rulesdata = [
         name="wallbox-entity-state-changes-dispatcher",
         filters=[
             IsSubscription(0),
-            GroupMatch(re.compile("^urmet\.wallbox\..+")),
+            # GroupMatch(re.compile("^urmet\.wallbox\..+")),
+            Filter(lambda payload: payload["group"] == "wallbox.chargers")
         ],
         processing=[
+            JsonDecode(properties=["ocpp_received", "ocpp_sent"]),
             Route(
                 topic="projects/krules-dev-254113/topics/wallbox-chargers-state-changes",
-                dispatch_policy=DispatchPolicyConst.DIRECT
+                dispatch_policy=DispatchPolicyConst.DIRECT,
+                payload=lambda payload: payload
             )
         ]
     )
--- deploy/ingestion/middlewares.py
+++ deploy/ingestion/middlewares.py
@@ -5,7 +5,8 @@ middlewares = [
         {
             "allow_origins": [
                 "http://localhost:4200",
-                "https://krules-app-companion-hgt7kizkdq-oc.a.run.app/"
+                "https://krules-app-companion-hgt7kizkdq-oc.a.run.app/",
+                "https://https://streams.krules.io/"
             ],
             "allow_credentials": True,
             "allow_methods": ["*"],
