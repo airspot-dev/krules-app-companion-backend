c5650ea208761ed298ad1128d7bff345fe8f4c33
Zenzo42 <lorenzo@airspot.tech>
2023-05-25 18:25:04 +0200

    add function to store data

---
+++ custom/deploy.sh
@@ -0,0 +1,2 @@
+firebase functions:config:set regions.0=europe-west1
+firebase deploy --only functions
--- custom/state-changes-archiver/deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud functions deploy state-changes-archiver --project airspot-krules-companion-dev --region europe-west6 \
-  --entry-point index \
-  --runtime python311 \
-  --trigger-event "providers/cloud.firestore/eventTypes/document.write" \
-  --trigger-resource "projects/airspot-krules-companion-dev/databases/(default)/documents/{subscription}/groups/{group_id}/{entity_id}"
